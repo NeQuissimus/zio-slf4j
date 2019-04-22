a9954489ce341c5e97400bded97bd4dbceabf95f
Robin Green <greenrd@greenrd.org>
2019-04-22 14:39:23 +0100

    Update to latest ZIO release candidate
    Remove console - no longer compatible, under the current design of the library.
    However, logback's default console logger can still be used.

--- src/main/scala/slf4j.scala
+++ src/main/scala/slf4j.scala
@@ -17,10 +17,10 @@ trait Slf4jLogger {
   implicit lazy val logger: Logger with InnerLogger[slf4j.Logger] = new Logger with InnerLogger[slf4j.Logger] {
     val inner: slf4j.Logger = slf4j.LoggerFactory.getLogger(clazzName)
 
-    def trace[A](a: A)(implicit S: Show[A]): IO[Throwable, Unit] = IO.syncThrowable(inner.trace(S.shows(a)))
-    def debug[A](a: A)(implicit S: Show[A]): IO[Throwable, Unit] = IO.syncThrowable(inner.debug(S.shows(a)))
-    def info[A](a: A)(implicit S: Show[A]): IO[Throwable, Unit]  = IO.syncThrowable(inner.info(S.shows(a)))
-    def warn[A](a: A)(implicit S: Show[A]): IO[Throwable, Unit]  = IO.syncThrowable(inner.warn(S.shows(a)))
-    def error[A](a: A)(implicit S: Show[A]): IO[Throwable, Unit] = IO.syncThrowable(inner.error(S.shows(a)))
+    def trace[A](a: A)(implicit S: Show[A]): IO[Nothing, Unit] = IO.effectTotal(inner.trace(S.shows(a)))
+    def debug[A](a: A)(implicit S: Show[A]): IO[Nothing, Unit] = IO.effectTotal(inner.debug(S.shows(a)))
+    def info[A](a: A)(implicit S: Show[A]): IO[Nothing, Unit]  = IO.effectTotal(inner.info(S.shows(a)))
+    def warn[A](a: A)(implicit S: Show[A]): IO[Nothing, Unit]  = IO.effectTotal(inner.warn(S.shows(a)))
+    def error[A](a: A)(implicit S: Show[A]): IO[Nothing, Unit] = IO.effectTotal(inner.error(S.shows(a)))
   }
 }
