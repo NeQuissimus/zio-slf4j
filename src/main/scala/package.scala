7ea5f7456e4957a126dc9ba260ffa1891fe34b71
Tim Steinbach <tim@nequissimus.com>
2019-08-03 11:19:26 -0400

    Make Scalaz optional
    Update ZIO to 1.0.0-RC10-1
    Fixes #27

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,12 +1,21 @@
 package nequi.zio
 
 import scalaz.Show
-import scalaz.zio.ZIO
+
+import zio.ZIO
 
 package object logger extends Logger.Service[Logger] {
-  final def trace[A: Show](a: A): ZIO[Logger, Nothing, Unit] = ZIO.accessM(_.logger.trace(a))
-  final def debug[A: Show](a: A): ZIO[Logger, Nothing, Unit] = ZIO.accessM(_.logger.debug(a))
-  final def info[A: Show](a: A): ZIO[Logger, Nothing, Unit]  = ZIO.accessM(_.logger.info(a))
-  final def warn[A: Show](a: A): ZIO[Logger, Nothing, Unit]  = ZIO.accessM(_.logger.warn(a))
-  final def error[A: Show](a: A): ZIO[Logger, Nothing, Unit] = ZIO.accessM(_.logger.error(a))
+  implicit class ScalazLogger(private val logger: Logger.Service[Logger]) {
+    final def trace[A](a: A)(implicit S: Show[A]): ZIO[Logger, Nothing, Unit] = logger.trace(S.shows(a))
+    final def debug[A](a: A)(implicit S: Show[A]): ZIO[Logger, Nothing, Unit] = logger.debug(S.shows(a))
+    final def info[A](a: A)(implicit S: Show[A]): ZIO[Logger, Nothing, Unit]  = logger.info(S.shows(a))
+    final def warn[A](a: A)(implicit S: Show[A]): ZIO[Logger, Nothing, Unit]  = logger.warn(S.shows(a))
+    final def error[A](a: A)(implicit S: Show[A]): ZIO[Logger, Nothing, Unit] = logger.error(S.shows(a))
+  }
+
+  final def trace(a: String): ZIO[Logger, Nothing, Unit] = ZIO.accessM(_.logger.trace(a))
+  final def debug(a: String): ZIO[Logger, Nothing, Unit] = ZIO.accessM(_.logger.debug(a))
+  final def info(a: String): ZIO[Logger, Nothing, Unit]  = ZIO.accessM(_.logger.info(a))
+  final def warn(a: String): ZIO[Logger, Nothing, Unit]  = ZIO.accessM(_.logger.warn(a))
+  final def error(a: String): ZIO[Logger, Nothing, Unit] = ZIO.accessM(_.logger.error(a))
 }
